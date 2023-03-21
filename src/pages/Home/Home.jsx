76fd9366118daa6a1e1ba0448aa35ad59edafc5f
Lukas Baksys <lubaksys@gmail.com>
2023-03-21 12:28:04 +0200

    small fix for deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../../components/card/Card";
+import Card from "../../components/Card/Card";
 import styles from "./Home.module.scss";
 import { Link } from "react-router-dom";
 import { Masonry } from "@mui/lab";
